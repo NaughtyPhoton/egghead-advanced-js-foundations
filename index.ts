b11991191e135b9f9ca97278c6be06d91746eddf
NaughtyPhoton <nathanstrong.fx@gmail.com>
2020-07-26 14:11:58 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,12 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+// Primitive Types
+console.log(typeof 'Hello World') // string
+console.log(typeof 42) // number
+console.log(typeof false) // boolean
+console.log(typeof 42n) // bigint
+console.log(typeof 'Hello World') // string
+console.log(typeof 'Hello World') // string
+console.log(typeof 'Hello World') // string
