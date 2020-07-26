452bf198f26159666fd89597d47a55cd9f5c6013
NaughtyPhoton <nathanstrong.fx@gmail.com>
2020-07-26 15:59:34 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -7,9 +7,64 @@ appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 // Primitive Types
 console.log(typeof 'Hello World') // string
-console.log(typeof 42) // number
-console.log(typeof false) // boolean
-console.log(typeof 42n) // bigint
-console.log(typeof 'Hello World') // string
-console.log(typeof 'Hello World') // string
-console.log(typeof 'Hello World') // string
+console.log(typeof 42); // number
+console.log(typeof false); // boolean
+console.log(typeof 42n); // bigint
+console.log(typeof Symbol()); // symbol
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// mutating object
+let myObj = {a: 1};
+
+// IIFE That mutates given object
+(function addTwo(obj) {
+  obj.a += 2;
+})(myObj);
+
+console.log(myObj);
+
+// Trying to mutate Primitive
+let num = 1;
+
+// IIFE That tries to mutates given number
+(function addTwo(num) {
+  num += 2;
+  console.log(num); // changes within this scope
+})(num);
+
+console.log(num);
+
+/**
+ * Understanding Prototypal Inheritance
+ */
+const a = {};
+console.log(a);
+
+const b = Object.create(a);
+
+a.cool = 'awesome';
+
+console.log(b);
+
+/**
+ * Functions are first class objects
+ */
+function foo() {}
+
+foo.firstName = 'bar';
+
+console.dir(foo);
+console.log(foo.prototype);
+console.log(foo.toString());
+
+/**
+ * Block scopes
+ */
+
+var firstName = 'tyler';
+{
+  var firstName = 'clark';
+  console.log(firstName);
+}
+console.log(firstName);
